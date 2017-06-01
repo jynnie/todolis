9e904af296d5831876f77cb1529ee8df7bfad86c
Jynnie <jynnie@mit.edu>
2017-06-01 11:15:37 -0400

    Startup framework with signup

---
+++ app.py
@@ -0,0 +1,59 @@
+from flask import Flask #importing the flask library
+from flask import render_template #allows for rendering of html files (with jinja)
+from flask import session, flash, request, redirect, abort #needed for creating user sessions (log ins)
+from wtforms import Form, TextField, validators, StringField, SubmitField #form security
+from sqlalchemy.orm import sessionmaker #needed in order to interact with database
+from tabledef import * #also needed in order to interact with database
+import os #used to generate secret key
+
+app = Flask(__name__) #initializes flask application
+app.config.from_object(__name__)
+
+engine = create_engine('sqlite:///tutorial.db', echo=True) #needed to interact with the database tutorial.db
+
+
+
+class UserForm(Form): #general form class for logins and registration
+    username = StringField('Username', validators=[validators.required()])
+    email = StringField('Email', validators=[validators.required()])
+    password = TextField('Password', validators=[validators.required(), validators.Length(min=3, max=35)])
+
+    def reset(self): #resets back to blank data and reinitializes the security
+        blankData = MultiDict([ ('csrf', self.reset_csrf()) ])
+        self.process(blankData)
+
+
+
+@app.route("/")
+@app.route("/index")
+def index(): #tells what flask should return/do when user accesses above routes
+    return render_template("index.html")
+
+@app.route("/signup", methods=['GET', 'POST'])
+def signup():
+    form = UserForm(request.form) #gets the data from the form submitted by the user
+    print form.errors
+
+    if request.method == 'POST':
+        Session = sessionmaker(bind=engine) #starts session in order to insert things into a database
+        s = Session()
+
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+        print username, " ", email, " ", password
+
+        if form.validate(): #if the form is validated then we want to add a new user to the database
+            user = User(username, email, password) #adding to the database User the following data
+            s.add(user)
+            s.commit()
+
+            flash('Hello ' + username + '! Thanks for registering')
+        else:
+            flash('Error: All fields of the form are required')
+
+    return render_template("signup.html",
+                            form=form) #so it will recognize that the form on the page is the same as the form defined in the signup
+
+app.secret_key = os.urandom(12) #essential to creating a session
+app.run(debug=True) #tells python to run the application with debugging on
---
+++ documentation.md
@@ -0,0 +1,63 @@
+#Making a Multi-User Todo List with Flask
+
+Show them what we're going to make;
+explain basic structure of how web apps work
+explain python, html, css, and jinja used
+
+Setup folders:
+todo
+todo > templates
+
+make app.py in todo
+
+1. import the flask library
+2. initialization
+3. setup index route
+4. setup app.run (then test)
+
+default will port to 5000
+okay we now have a basic page
+lets setup our first html page
+
+5. templates > base.html
+	- explain tags with the html tag (explain what html is)
+	- setup header (explain what a header is)
+	- setup body
+	- block content (explain)
+6. templates > index.html
+	- show extend
+	- put something basic in
+7. import render_template + edit index route
+	- show and explain
+8. add bootstrap and wow them
+	- show the immediate change
+9. use style to stick footer to the bottom
+	- just to show them a sample of how styling can happen;
+	  more to come in the future, this is just a taste
+	  let's finish giving the app functionality
+
+(^ the above took approx. 15 minutes)
+
+10. start setting up signup routes
+	- need to add get and post methods (must explain)
+	- should also explain how a login system 
+	  will work with databases before actually just doing it
+11. create signup.html
+	- need to explain how forms work
+	- need to explain id's v. class's
+	- now we have the front end we need to make this actually do things
+	- also add this to the navbar
+12. import wtforms
+13. Make UserForm class
+14. add functionality to sign up
+	- remember to add back form.csrf
+	- remember to add form=form to render_template
+	- remember to add secret_key
+15. make database with tabledef.py
+	- well now we need to actually post these things into a database
+	- need to explain how __init__ works
+	- need to explain the difference between User() and users
+	- need to explain columns (draw for this)
+16. create database by running tabledef.py
+17. test sign up
+18. add flash prompts
